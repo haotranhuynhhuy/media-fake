9e204ee2b3ac8a6381284d0f288737f1c5d3e8fd
haotranhuynhhuy <tranhuynhhuyhao@gmail.com>
2023-04-01 15:31:09 +0700

    refactor auth with context graphql

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,24 +1,24 @@
+import { AuthenticationError } from "apollo-server-express";
 import jwt from "jsonwebtoken";
 
-const verifyToken = (req, res, next) => {
-  const authHeader = req.header("Authorization");
+const verifyToken = (context) => {
+  // context header
+  const authHeader = context.req.header("Authorization");
   const token = authHeader && authHeader.split(" ")[1];
 
   //Check access token
   if (!token) {
-    return res
-      .status(403)
-      .json({ success: false, message: "Access token denied" });
+    throw new AuthenticationError(
+      "Authentication token must be 'Bearer [token]"
+    );
   }
-  //All good
 
+  //All good
   try {
-    const decode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    req.userId = decode.userId;
-    next();
+    const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    return user;
   } catch (error) {
-    console.log(error);
-    return res.status(403).json({ success: false, message: "Invalid token" });
+    throw new AuthenticationError("Authorization header must be provided");
   }
 };
 export default verifyToken;
