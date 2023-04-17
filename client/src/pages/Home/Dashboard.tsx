8ed1ef7b806db9a8f9be7996d1a063d2add88e8a
haotranhuynhhuy <tranhuynhhuyhao@gmail.com>
2023-04-17 15:34:20 +0700

    card notify

--- client/src/pages/Home/Dashboard.tsx
+++ client/src/pages/Home/Dashboard.tsx
@@ -4,8 +4,36 @@ import Image from "../../components/BaseComponents/Image";
 import image from "../../assets/react.svg";
 import Dropdown from "../../components/BaseComponents/Dropdown";
 import IconTypo from "../../components/BaseComponents/IconTypo";
+import alarm from "../../assets/images/alarmSVG.svg";
+import CardNotifyList, {
+  NewsListCardProps,
+} from "../../components/BaseComponents/CardNotify";
 
 const Dashboard = () => {
+  const [cardListnotify, setCardListnotify] = useState<NewsListCardProps[]>([
+    {
+      imgSrc: alarm,
+      nameUser: "",
+      title:
+        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur alias corporis vero. Velit minus quisquam, eveniet voluptatum magni provident autem! Qui delectus corrupti, error eum hic dolore. Rerum, nesciunt libero.",
+      date: new Date(),
+      handleClick: () => {},
+      href: "/",
+      isVertical: true,
+    },
+    {
+      imgSrc: alarm,
+      nameUser: "",
+      title:
+        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur alias corporis vero. Velit minus quisquam, eveniet voluptatum magni provident autem! Qui delectus corrupti, error eum hic dolore. Rerum, nesciunt libero.",
+      date: new Date(),
+      handleClick: () => {},
+      href: "/",
+      isVertical: true,
+    },
+  ]);
+  const [isOpenNotify, setisShowNotify] = useState(true);
+
   const [optionSideBarMobile, setOptionSideBarMobile] = useState([
     {
       id: 1,
@@ -58,6 +86,11 @@ const Dashboard = () => {
         name="Hao tran"
         handleSeeMore={handleSeeMore}
       />
+      <div>
+        {isOpenNotify && (
+          <CardNotifyList title="Thông báo" notifyList={cardListnotify} />
+        )}
+      </div>
     </div>
   );
 };
